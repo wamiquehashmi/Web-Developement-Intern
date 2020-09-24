4f5d78bd3cac11c7a32274c5c5992696601deb28
Wamique Hashmi <wamiquehashmi@outlook.com>
2020-09-24 08:54:38 +0530

    Update kelvin.js

--- JavaScript/Project Task 6/kelvin.js
+++ JavaScript/Project Task 6/kelvin.js
@@ -1,14 +1,14 @@
 // Today's Weather in kelvin
 var kelvin= 293;
 // converting temperature from kelvin to celsius
-var celsius = kelvin-273
+var celsius = kelvin-273;
 //converting temperature from celsius to fahrenheit
-var fahrenheit = celsius * (9/5) + 32
+var fahrenheit = celsius * (9/5) + 32;
 //rounding down the decimal value of fahrenheit to integer
-fahrenheit = Math.floor(fahrenheit)
-console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`)
+fahrenheit = Math.floor(fahrenheit);
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
 //converting temperature from celsius to Newton scale
-var newton = celsius * (33/100)
+var newton = celsius * (33/100);
 //rounding down the decimal value of newton to integer
-newton = Math.floor(newton)
-console.log(`The temperature is ${newton} degrees Newton.`)
+newton = Math.floor(newton);
+console.log(`The temperature is ${newton} degrees Newton.`);

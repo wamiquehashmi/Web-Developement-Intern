f3dcdb452101cc16ea26574fc0cd177c10c73b2f
Wamique Hashmi <wamiquehashmi@outlook.com>
2020-09-26 22:59:22 +0530

    Create BoxModel.js

---
+++ JavaScript/Project Task 11/BoxModel.js
@@ -0,0 +1,20 @@
+document.querySelector("img").setAttribute("width","180");
+document.querySelector("nav img").style.margin="0% 43.5%";
+document.querySelector("nav span").style.margin="10px 0px";
+var content = document.querySelector(".content");
+content.style.height="500px";
+content.style.margin="10px auto";
+content.style.overflow="scroll";
+document.querySelector(".header").style.height="320px";
+document.querySelector(".header h1").style.padding="20px";
+document.querySelector(".header h1").style.margin="0px auto";
+var button = document.querySelector(".button");
+button.style.width="200px";
+button.style.padding="20px"
+button.style.margin="40px auto"
+button.style.border="1px solid blue"
+document.querySelector("ul.nutrition").style.padding="20px";
+var li = document.querySelector("ul.nutrition li");
+li.style.height="200px";
+li.style.padding="10px 20px"
+li.style.margin="0px 0px 3px 0px"

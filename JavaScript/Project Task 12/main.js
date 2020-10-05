df419bb787ee20fe0d3d2c4730c7d1c19e7c727c
Wamique Hashmi <wamiquehashmi@outlook.com>
2020-10-05 22:44:12 +0530

    Update main.js

--- JavaScript/Project Task 12/main.js
+++ JavaScript/Project Task 12/main.js
@@ -1,5 +1,5 @@
-document.querySelector("header").style.position="fixed"
-document.querySelector("header").style.width="1349px"
+document.querySelector("header").style.position="fixed";
+document.querySelector("header").style.width="1349px";
 var li = document.querySelectorAll("nav li");
 for(let i=0; i<li.length; i++){
 	li[i].style.display="inline";
@@ -9,15 +9,15 @@ for(let i=0; i<li.length; i++){
 
 document.querySelector("main").style.position="relative";
 document.querySelector("header").style.zIndex="1";
-document.querySelector("main").offsetTop="80px"
+document.querySelector("main").offsetTop="80px";
 document.querySelector("main").style.top="80px";
 
-var dimension = document.querySelectorAll(".supporting .col")
+var dimension = document.querySelectorAll(".supporting .col");
 for(let i=0; i<dimension.length; i++){
 	dimension[i].style.width="200px";
 	dimension[i].style.height="200px";
 }
-var display = document.querySelectorAll(".supporting .col")
+var display = document.querySelectorAll(".supporting .col");
 for(let i=0; i<display.length; i++){
 	display[i].style.display="inline-block";
 }
